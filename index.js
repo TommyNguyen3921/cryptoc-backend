7e69ed3fa4e4b6f1f6c3889a8353835252474de1
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-12 01:10:46 -0500

    api working2

--- index.js
+++ index.js
@@ -23,6 +23,6 @@ app.get('/',(req,res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology:true })
-.then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.then(() => app.listen(PORT,'0.0.0.0', () => console.log(`Server running on port: ${PORT}`)))
 .catch((error) => console.log(error.message));
 
