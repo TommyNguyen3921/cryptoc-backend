87a11eaf6066bdcc537da7e2906fc3141724f2a0
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-14 03:41:41 -0400

    users

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-
+import userRoutes from './routes/users.js';
 
 const app = express();
 //posts will be the start in the routes
@@ -15,6 +15,7 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/users', userRoutes);
 
 app.get('/',(req,res) => {
     res.send('hello to crypto api');
