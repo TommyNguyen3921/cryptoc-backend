74c38d4f49e1950d13d9dea871dfc25f6a5b94f6
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-14 04:14:09 -0400

    users

--- controllers/user.js
+++ controllers/user.js
@@ -9,21 +9,38 @@ export const signin = async (req,res) => {
 
     try {
         const existinguser = await User.findOne({email});
-        console.log(postMessage);
+        
+        if (!existinguser) return res.status(404).json({message: "User does not exist"});
 
-        res.status(200).json(postMessage);
+        const isPasswordCorrect = await bcrypt.compare(password, existinguser.password);
+
+        if (!isPasswordCorrect) return res.status(400).json({message: "Invalid credential"});
+
+        const token = jwt.sign({email: existinguser.email, id: existinguser._id}, 'test',{expiresIn: "1h"});
+
+        return res.status(200).json({result: existinguser, token});
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(500).json({message: 'Something went wrong.'})
     }
 }
 
 export const signup = async (req,res) => {
-    try {
-        const postMessage = await PostMessage.find();
-        console.log(postMessage);
 
-        res.status(200).json(postMessage);
+    const {email, password, firstName, lastName} = req.body;
+
+    try {
+        const existinguser = await User.findOne({email});
+        if (existinguser) return res.status(400).json({message: "User already exist"});
+
+        //hashpassword 12 is the salt in the password
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+
+        const result = new User({email,password: hashedPassword, name: `${firstName} ${lastName}`})
+
+        const token = jwt.sign({email: result.email, id: result._id}, 'test',{expiresIn: "1h"});
+        return res.status(200).json({result, token});
     } catch (error) {
-        res.status(404).json({message: error.message})
+        res.status(500).json({message: 'Something went wrong.'})
     }
 }
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
 import {getPosts,createPosts, deletePost} from '../controllers/posts.js'
+
+//video 1:50:09 for auth for fav
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
 
 //this rotues would be localhost:500/posts
