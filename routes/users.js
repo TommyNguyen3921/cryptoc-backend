49495b0b00a6123edef60dc5fecf524bd5ef3650
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-29 00:36:11 -0400

    middleware

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,19 @@
 import express from 'express';
 
 import {signin,signup,addfav,getPosts,deletePost} from '../controllers/user.js'
+import auth from '../middleware/auth.js';
+
 const router = express.Router();
+
+//video 1:50:09 for auth for fav
 
 //this rotues would be localhost:500/posts
 
 
 router.post('/signin',signin);
 router.post('/signup',signup);
-router.patch('/:id',addfav);
-router.get('/:id',getPosts);
-router.patch('/remove/:id', deletePost);
+router.patch('/:id',auth,addfav);
+router.get('/:id',auth,getPosts);
+router.patch('/remove/:id',auth, deletePost);
 
 export default router
