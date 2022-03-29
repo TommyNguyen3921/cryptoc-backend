6e9463b6cf9268b26de2d373b1d0f316375f76da
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-29 00:04:06 -0400

    delete post

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 
 const app = express();
@@ -14,7 +13,7 @@ app.use(bodyParser.json({limit:"30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended: true}));
 app.use(cors());
 
-app.use('/posts', postRoutes);
+
 app.use('/users', userRoutes);
 
 app.get('/',(req,res) => {
