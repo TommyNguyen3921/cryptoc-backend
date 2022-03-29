8ad4d7895a0c21b31bb6b473043e2ce6e1d05ed3
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-29 01:56:28 -0400

    middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,11 +1,9 @@
 import jwt from 'jsonwebtoken';
 
-
 const auth = async (req, res, next) => {
     try {
+        const token = req.headers.Authorization.split(" ")[1];
         
-        const authh = req.headers["authorization"];
-        const token = authh && authh.split(' ')[1];
         let decodedData = jwt.verify(token, 'test');
 
         req.userId = decodedData?.id;
