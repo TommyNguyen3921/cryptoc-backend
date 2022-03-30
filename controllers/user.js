02eba13ddfe790089aca1240281e66189cdbd4b3
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-29 23:54:16 -0400

    secret env

--- controllers/user.js
+++ controllers/user.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/user.js';
-import usermodel from '../models/user.js';
+
 
 export const signin = async (req,res) => {
 
@@ -18,7 +18,7 @@ export const signin = async (req,res) => {
 
         if (!isPasswordCorrect) return res.status(400).json({message: "Invalid credential"});
 
-        const token = jwt.sign({email: existinguser.email, id: existinguser._id}, 'test',{expiresIn: "1h"});
+        const token = jwt.sign({email: existinguser.email, id: existinguser._id}, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "1h"});
 
         return res.status(200).json({result: existinguser, token});
     } catch (error) {
@@ -43,7 +43,7 @@ export const signup = async (req,res) => {
 
        
         
-        const token = jwt.sign({email: result.email, id: result._id}, 'test',{expiresIn: "1h"});
+        const token = jwt.sign({email: result.email, id: result._id}, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "1h"});
         return res.status(200).json({result, token});
     } catch (error) {
         res.status(500).json({message: 'Something went wrong.'})
