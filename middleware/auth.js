c6d42cc4dc6ac820adcad8ca1e902833e73171ce
TommyNguyen3921 <55663532+TommyNguyen3921@users.noreply.github.com>
2022-03-29 23:56:41 -0400

    secret env

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,7 +5,7 @@ const auth = async (req, res, next) => {
         
         const token = req.headers.authorization.split(" ")[1];
         
-        let decodedData = jwt.verify(token, 'test');
+        let decodedData = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         req.userId = decodedData?.id;
 
